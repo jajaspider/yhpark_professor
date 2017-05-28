4184d3ffc60ec13ccd4097a145c878b33fa4de48
Sonagod <jaja_spider@naver.com>
2017-05-28 17:29:51 +0900

    수정중

--- yhpark/models.py
+++ yhpark/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Professor(models.Model):
-    author = models.ForeignKey()
+    author = models.CharField(max_length=7)
     force = models.CharField(max_length=500)
     research = models.CharField(max_length=500)
     academy = models.CharField(max_length=500)
