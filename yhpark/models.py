5c99e582bce9692d55eca883f6e3b4bef95ec65c
Sonagod <jaja_spider@naver.com>
2017-05-28 17:22:44 +0900

    수정중

--- yhpark/models.py
+++ yhpark/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class  Professor(models.Model):
+    author = models.ForeignKey('auth.User')
+    force = models.CharField()
+    research = models.CharField()
+    academy = models.CharField()
+    awards = models.CharField()
+    student = models.CharField()
