216b9d3b085947f15700cc2cca01bec2440aec51
Sonagod <jaja_spider@naver.com>
2017-05-28 17:30:58 +0900

    수정중

--- yhpark/models.py
+++ yhpark/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Professor(models.Model):
     author = models.CharField(max_length=7)
     force = models.CharField(max_length=500)
-    research = models.CharField(max_length=500)
-    academy = models.CharField(max_length=500)
-    awards = models.CharField(max_length=500)
-    student = models.CharField(max_length=500)
+    research = models.TextField(max_length=500)
+    academy = models.TextField(max_length=500)
+    awards = models.CharField(TextField=500)
+    student = models.CharField(TextField=500)
